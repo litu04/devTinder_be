cd0972f791ba45595e6188704df700d33ae922ee
litu04 <debadattakar96@gmail.com>
2025-06-24 13:39:28 +0530

    "playing with get, post and delete api"

--- src/app.js
+++ src/app.js
@@ -14,6 +14,19 @@ app.use("/hello", (req, res) => {
     res.send("hello from the server!");
 })
 
+app.get("/user", (req, res) => {
+    res.send("get all users");
+});
+
+app.post("/user", (req, res) => {
+    console.log("req body----> ", req.body);
+    res.send("Post all users");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("delete all users");
+});
+
 app.listen(8000, () => {
     console.log("server is up and running on PORT: 8000");
 });
